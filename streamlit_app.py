0be6ae17ec99e796b24141ce6f01f7f655994da1
sweta0206 <tripathisweta571@gmail.com>
2025-10-30 17:27:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ ingredients_list= st.multiselect(
 
 
 if ingredients_list:
-    ingredients_string = ','.join(ingredients_list)
+    ingredients_string = ' '.join(ingredients_list)
     
     for fruit_chosen in ingredients_list:
        #ingredients_string += fruit_chosen + ' '
